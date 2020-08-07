d04f0807b8cdea028a4466b4b4c81a3ec0a5a675
djlolepop <67583626+djlolepop@users.noreply.github.com>
2020-08-07 17:42:10 +0530

    adding night
    adding night

--- Bird.js
+++ Bird.js
@@ -2,20 +2,24 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
-    this.smokeimg = loadImage("sprites/smoke.png");
-    this.tj = []
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory =[];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
-    var pos = [this.body.position.x,this.body.position.y]
-    this.tj.push(pos);
-if (this.body.position.x>200&&this.body.velocity.x>10) {
-	for (var i = 0; i <this.tj.length ; i++) {
-image(this.smokeimg,this.tj[i][0],this.tj[i][1])
-}
-  
+
+    super.display();
+
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
+    }
+   
+
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
+    }
   }
-  super.display();
-}}
+}
